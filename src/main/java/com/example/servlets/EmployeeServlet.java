9aedac63a350eaa3bb32e07831c8840bc2105b75
Mario <mario@gmail.com>
2025-02-03 22:03:55 +0200

    lesson_03022025

---
+++ src/main/java/com/example/servlets/EmployeeServlet.java
@@ -0,0 +1,21 @@
+package com.example.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class EmployeeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        System.out.println(req.getServletPath());
+        RequestDispatcher rd = req.getRequestDispatcher("employee.jsp");
+        rd.forward(req, resp);
+    }
+}
